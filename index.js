3dbb79ee0a79f5abbcfdea9f98d7edd9e399947e
shankar1525 <55125365+shankar1525@users.noreply.github.com>
2022-12-08 18:25:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var http = require('http'); // 1 - Import Node.js core module
+
+var server = http.createServer(function (req, res) {   // 2 - creating server
+
+    //handle incomming requests here..
+    if (req.url == '/') { //check the URL of the current request
+        
+        // set response header
+        res.writeHead(200, { 'Content-Type': 'text/html' }); 
+        
+        // set response content    
+        res.write('<html><body><p>This is home Page.</p></body></html>');
+        res.end();
+    
+    }
+
+});
+
+server.listen(3002); //3 - listen for any incoming requests
+
+console.log('Node.js web server at port 3002 is running..')
